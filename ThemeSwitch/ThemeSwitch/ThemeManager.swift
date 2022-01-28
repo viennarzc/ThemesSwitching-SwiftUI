72599783fca627d028a339c90002c44d6fadefac
Viennarz Curtiz <viennarzc@gmail.com>
2022-01-28 17:56:09 +0800

    Create Color Assets, create protocol Theme

---
+++ ThemeSwitch/ThemeSwitch/ThemeManager.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeManager.swift
+//  ThemeSwitch
+//
+//  Created by Viennarz Curtiz on 1/28/22.
+//
+
+import Foundation
---
+++ ThemeSwitch/ThemeSwitch/Themes.swift
@@ -0,0 +1,45 @@
+//
+//  Themes.swift
+//  ThemeSwitch
+//
+//  Created by Viennarz Curtiz on 1/28/22.
+//
+
+import SwiftUI
+
+protocol Theme {
+    var primaryColor: Color { get }
+    var secondaryColor: Color { get }
+    var backgroundColor: Color { get }
+    var accentColor: Color { get }
+    var foregroundColor: Color { get }
+}
+
+struct Theme1: Theme {
+    var foregroundColor: Color = Color("1-ForegroundColor")
+    
+    var primaryColor: Color = Color("1-PrimaryColor")
+    
+    var secondaryColor: Color = Color("1-SecondaryColor")
+    
+    var backgroundColor: Color = Color("1-BackgroundColor")
+    
+    var accentColor: Color = Color("1-AccentColor")
+    
+    
+}
+
+struct Theme2: Theme {
+    var foregroundColor: Color = Color("2-ForegroundColor")
+    
+    var primaryColor: Color = Color("2-PrimaryColor")
+    
+    var secondaryColor: Color = Color("2-SecondaryColor")
+    
+    var backgroundColor: Color = Color("2-BackgroundColor")
+    
+    var accentColor: Color = Color("2-AccentColor")
+    
+    
+    
+}
