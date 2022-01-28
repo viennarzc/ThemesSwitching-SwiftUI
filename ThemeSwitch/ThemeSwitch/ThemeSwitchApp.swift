0f65fab0069622d9f529975721829e2d6dc9f290
Viennarz Curtiz <viennarzc@gmail.com>
2022-01-28 17:26:06 +0800

    Create Project

---
+++ ThemeSwitch/ThemeSwitch/ThemeSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeSwitchApp.swift
+//  ThemeSwitch
+//
+//  Created by Viennarz Curtiz on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ThemeSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
